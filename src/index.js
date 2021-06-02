a35831e2aba8839278e22552381b38913672fb5c
hadi <hadi_960@hotmail.com>
2021-06-01 22:25:32 -0500

    adds eventlistner for button

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 const Base_URL = "https://swapi.py4e.com/api/vehicles/?format=json"
 
 document.addEventListener("DOMContentLoaded",  () => {
-    console.log("Hello World")
+    // document.getElementById("vehicle").addEventListener("click", renderCar)
+    // console.log("Hello World")
 getCar()
+btnCar()
 })
-
 const getCar = () => {
     fetch(Base_URL)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data.results[0])
+        data.results.forEach(renderCar)
+    })
 }
-
-
+const btnCar = (data) => {
+    const btn1 = document.getElementById("car-list")
+    btn1.addEventListener("click",(e) => {
+        e.preventDefault()
+        // console.log("test")
+    })
+}
+const renderCar = (data) => {
+    let ul = document.getElementById("vehicle")
+    // ul.innerHTML = ""
+}
